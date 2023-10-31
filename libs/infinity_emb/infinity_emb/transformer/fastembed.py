3974581cdbb98ab8b7385014e607d0e632350cdf
michaelfeil <me@michaelfeil.eu>
2023-10-31 00:00:00 +0000

    initial commit

--- libs/infinity_emb/infinity_emb/transformer/fastembed.py
+++ libs/infinity_emb/infinity_emb/transformer/fastembed.py
@@ -1,52 +1,63 @@
-# from typing import List, Dict
-# from infinity_emb.inference.primitives import NpEmbeddingType
-# from infinity_emb.transformer.abstract import BaseTransformer
-# import numpy as np
-# import copy
+import copy
+from typing import Dict, List
 
-# class FlagEmbeddingFake:
-#     def __init__(self, *args, **kwargs) -> None:
-#         pass
+import numpy as np
 
-# try:
-#     from fastembed.embedding import FlagEmbedding, normalize
-# except:
-#     FlagEmbedding = FlagEmbeddingFake
+from infinity_emb.inference.primitives import NpEmbeddingType
+from infinity_emb.transformer.abstract import BaseTransformer
 
-# class FastEmbed(FlagEmbedding, BaseTransformer):
-#     def __init__(self, *args, **kwargs):
-#         FlagEmbedding.__init__(self)(*args, **kwargs)
-#         if FlagEmbedding == FlagEmbeddingFake:
-#             raise ImportError("fastembed is not installed.")
-#         self._infinity_tokenizer = copy.deepcopy(self.tokenizer)
+try:
+    from fastembed.embedding import DefaultEmbedding, normalize
 
-#     def encode_pre(self, sentences: List[str]) -> Dict[str, np.ndarray[int]]:
-#         encoded = self.tokenizer.encode_batch(sentences)
-#         input_ids = np.array([e.ids for e in encoded])
-#         attention_mask = np.array([e.attention_mask for e in encoded])
+    FASTEMBED_AVAILABLE = True
+except ImportError:
+    FASTEMBED_AVAILABLE = False
 
-#         onnx_input = {
-#             "input_ids": np.array(input_ids, dtype=np.int64),
-#             "attention_mask": np.array(attention_mask, dtype=np.int64),
-#         }
+    class DefaultEmbedding:
+        def __init__(self, *args, **kwargs) -> None:
+            pass
 
-#         if not self.exclude_token_type_ids:
-#             onnx_input["token_type_ids"] = np.array(
-#                 [np.zeros(len(e), dtype=np.int64) for e in input_ids], dtype=np.int64
-#             )
-#         return onnx_input
 
-#     def encode_core(self, features: Dict[str, np.ndarray[int]]) -> np.ndarray:
-#         model_output = self.model.run(None, features)
-#         last_hidden_state = model_output[0][:, 0]
-#         return last_hidden_state
+class Fastembed(DefaultEmbedding, BaseTransformer):
+    def __init__(self, *args, **kwargs):
+        if not FASTEMBED_AVAILABLE:
+            raise ImportError(
+                "fastembed is not installed." "`pip install infinity-emb[fastembed]`"
+            )
+        if not kwargs.get("cache_dir"):
+            from infinity_emb.transformer.utils import infinity_cache_dir
 
-#     def encode_post(self, embedding: np.ndarray) -> NpEmbeddingType:
-#         return normalize(embedding).astype(np.float32)
+            kwargs["cache_dir"] = infinity_cache_dir()
+        super(DefaultEmbedding, self).__init__(*args, **kwargs)
+        self._infinity_tokenizer = copy.deepcopy(self.model.tokenizer)
 
-#     def tokenize_lengths(self, sentences: List[str]) -> List[int]:
-#         # tks = self._infinity_tokenizer.encode_batch(
-#         #     sentences,
-#         # )
-#         # return [len(t.tokens) for t in tks]
-#         return [len(s) for s in sentences]
+    def encode_pre(self, sentences: List[str]) -> Dict[str, np.ndarray[int]]:
+        encoded = self.model.tokenizer.encode_batch(sentences)
+        input_ids = np.array([e.ids for e in encoded])
+        attention_mask = np.array([e.attention_mask for e in encoded])
+
+        onnx_input = {
+            "input_ids": np.array(input_ids, dtype=np.int64),
+            "attention_mask": np.array(attention_mask, dtype=np.int64),
+        }
+
+        if not self.model.exclude_token_type_ids:
+            onnx_input["token_type_ids"] = np.array(
+                [np.zeros(len(e), dtype=np.int64) for e in input_ids], dtype=np.int64
+            )
+        return onnx_input
+
+    def encode_core(self, features: Dict[str, np.ndarray[int]]) -> np.ndarray:
+        model_output = self.model.model.run(None, features)
+        last_hidden_state = model_output[0][:, 0]
+        return last_hidden_state
+
+    def encode_post(self, embedding: np.ndarray) -> NpEmbeddingType:
+        return normalize(embedding).astype(np.float32)
+
+    def tokenize_lengths(self, sentences: List[str]) -> List[int]:
+        # tks = self._infinity_tokenizer.encode_batch(
+        #     sentences,
+        # )
+        # return [len(t.tokens) for t in tks]
+        return [len(s) for s in sentences]
--- libs/infinity_emb/infinity_emb/transformer/utils.py
+++ libs/infinity_emb/infinity_emb/transformer/utils.py
@@ -1,7 +1,10 @@
+import os
 from enum import Enum
+from pathlib import Path
 from typing import Callable, Dict, List, Tuple
 
 from infinity_emb.transformer.dummytransformer import DummyTransformer
+from infinity_emb.transformer.fastembed import Fastembed
 from infinity_emb.transformer.sentence_transformer import (
     CT2SentenceTransformer,
     SentenceTransformerPatched,
@@ -19,6 +22,7 @@ __all__ = [
 class InferenceEngine(Enum):
     torch = SentenceTransformerPatched
     ctranslate2 = CT2SentenceTransformer
+    fastembed = Fastembed
     debugengine = DummyTransformer
 
 
@@ -37,3 +41,36 @@ def get_lengths_with_tokenize(
 ) -> Tuple[List[int], int]:
     _lengths = tokenize(_sentences)
     return _lengths, sum(_lengths)
+
+
+def infinity_cache_dir(overwrite=False):
+    """gets the cache dir. If
+
+    Args:
+        overwrite (bool, optional): _description_. Defaults to True.
+
+    Returns:
+        _type_: _description_
+    """
+    cache_dir = None
+    inf_home = os.environ.get("INFINITY_HOME")
+    st_home = os.environ.get("SENTENCE_TRANSFORMERS_HOME")
+    hf_home = os.environ.get("HF_HOME")
+    if inf_home:
+        cache_dir = inf_home
+    elif st_home:
+        cache_dir = st_home
+    elif hf_home:
+        cache_dir = hf_home
+    else:
+        cache_dir = Path(".").resolve() / ".infinity_cache"
+
+    if overwrite:
+        os.environ.setdefault("INFINITY_HOME", cache_dir)
+        os.environ.setdefault("SENTENCE_TRANSFORMERS_HOME", cache_dir)
+        os.environ.setdefault("HF_HOME", cache_dir)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir, exist_ok=True)
+
+    return cache_dir
