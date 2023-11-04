ff3554d97b9589b9539e3f8f542c24fe315dbe34
Michael Feil <michael.feil@michaelfeil.eu>
2023-11-04 15:08:31 +0100

    update test

--- libs/infinity_emb/tests/unit_test/test_infinity_server.py
+++ libs/infinity_emb/tests/unit_test/test_infinity_server.py
@@ -1,10 +1,16 @@
 import subprocess
 
-from fastapi import FastAPI
-import uvicorn
 import typer
-from infinity_emb.infinity_server import create_server, start_uvicorn, InferenceEngineTypeHint, UVICORN_LOG_LEVELS, cli
+import uvicorn
+from fastapi import FastAPI
 
+from infinity_emb.infinity_server import (
+    UVICORN_LOG_LEVELS,
+    InferenceEngineTypeHint,
+    cli,
+    create_server,
+    start_uvicorn,
+)
 from infinity_emb.transformer.utils import InferenceEngine
 
 
@@ -12,8 +18,9 @@ def test_cli_help():
     log = subprocess.run(["infinity_emb", "--help"])
     assert log.returncode == 0
 
+
 def test_patched_cli_help(mocker):
-    mocker.patch('typer.run')
+    mocker.patch("typer.run")
     cli()
     typer.run.assert_called_once_with(start_uvicorn)
 
@@ -21,18 +28,16 @@ def test_patched_cli_help(mocker):
 def test_cli_wrong_batch_size():
     log = subprocess.run(["infinity_emb", "--batch-size", "WrongArgument"])
     assert log.returncode == 2
-    stub.assert_called_once_with('foo', 'bar')
-
 
 def test_create_server():
     app = create_server(engine=InferenceEngine.debugengine)
     assert isinstance(app, FastAPI)
 
+
 def test_patched_create_uvicorn(mocker):
-    mocker.patch('uvicorn.run')
+    mocker.patch("uvicorn.run")
     start_uvicorn(
         log_level=UVICORN_LOG_LEVELS.debug,  # type: ignore
-        engine = InferenceEngineTypeHint.torch, 
+        engine=InferenceEngineTypeHint.torch,
     )
     assert uvicorn.run.call_count == 1
-    
