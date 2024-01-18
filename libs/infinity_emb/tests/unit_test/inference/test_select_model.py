75101bddb9ed33f3a8c99ccb4876e1b36b69be79
michaelfeil <me@michaelfeil.eu>
2024-01-18 21:46:22 +0100

    reformat

--- libs/infinity_emb/tests/unit_test/inference/test_select_model.py
+++ libs/infinity_emb/tests/unit_test/inference/test_select_model.py
@@ -1,14 +1,17 @@
 import pytest
 
 from infinity_emb.inference.select_model import select_model
-from infinity_emb.transformer.utils import InferenceEngine
 from infinity_emb.primitives import Device
+from infinity_emb.transformer.utils import InferenceEngine
+
 
 @pytest.mark.parametrize("engine", [e for e in InferenceEngine])
 def test_engine(engine):
     select_model(
         engine=engine,
-        model_name_or_path="BAAI/bge-small-en-v1.5" if engine == InferenceEngine.fastembed else pytest.DEFAULT_BERT_MODEL ,
+        model_name_or_path="BAAI/bge-small-en-v1.5"
+        if engine == InferenceEngine.fastembed
+        else pytest.DEFAULT_BERT_MODEL,
         batch_size=4,
         device=Device.cpu,
     )
