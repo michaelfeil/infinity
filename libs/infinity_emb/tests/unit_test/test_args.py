be9be2ded83059571aa2e1721748e2eeeb74cbe8
michaelfeil <me@michaelfeil.eu>
2024-02-17 18:12:07 -0800

    add engine args similar to vllm

---
+++ libs/infinity_emb/tests/unit_test/test_args.py
@@ -0,0 +1,31 @@
+from infinity_emb.args import EngineArgs
+from infinity_emb.primitives import Device
+from infinity_emb.transformer.utils import InferenceEngine
+
+
+def test_EngineArgs_no_input():
+    EngineArgs()
+
+
+def test_engine_args():
+    args = EngineArgs(
+        model_name_or_path="BAAI/bge-small-en-v1.5",
+        batch_size=64,
+        revision=None,
+        trust_remote_code=True,
+        engine="torch",
+        model_warmup=False,
+        vector_disk_cache_path="",
+        device="cpu",
+        lengths_via_tokenize=False,
+    )
+
+    assert args.model_name_or_path == "BAAI/bge-small-en-v1.5"
+    assert args.batch_size == 64
+    assert args.revision is None
+    assert args.trust_remote_code
+    assert args.engine == InferenceEngine.torch
+    assert not args.model_warmup
+    assert args.vector_disk_cache_path == ""
+    assert args.device == Device.cpu
+    assert not args.lengths_via_tokenize
