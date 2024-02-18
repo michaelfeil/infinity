8b9cc8677789012b8b62e7f2d8fd8d7fad5147de
michaelfeil <me@michaelfeil.eu>
2024-02-18 00:16:39 -0800

    update hf_transfer improvement

--- libs/infinity_emb/infinity_emb/transformer/__init__.py
+++ libs/infinity_emb/infinity_emb/transformer/__init__.py
@@ -1,2 +1,12 @@
 __all__ = ["InferenceEngine"]
 from infinity_emb.transformer.utils import InferenceEngine
+
+# place the enabeling of hf hub transfer here
+try:
+    # enable hf hub transfer if available
+    import hf_transfer  # type: ignore # noqa
+    import huggingface_hub.constants  # type: ignore
+
+    huggingface_hub.constants.HF_HUB_ENABLE_HF_TRANSFER = True
+except ImportError:
+    pass
