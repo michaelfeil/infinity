bfdcdaa3c8b02e8200a21afc2e5d4b62f035f877
michaelfeil <me@michaelfeil.eu>
2024-03-12 03:18:07 -0700

    fix model pipe macos

--- libs/infinity_emb/tests/end_to_end/test_optimum_embedding.py
+++ libs/infinity_emb/tests/end_to_end/test_optimum_embedding.py
@@ -1,5 +1,3 @@
-import sys
-
 import pytest
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
@@ -59,7 +57,6 @@ async def test_embedding(client, model_base, helpers):
 
 @pytest.mark.performance
 @pytest.mark.anyio
-@pytest.mark.skipif(sys.platform == "darwin", reason="Skip on macOS")
 async def test_batch_embedding(client, get_sts_bechmark_dataset, model_base, helpers):
     await helpers.util_batch_embedding(
         client=client,
