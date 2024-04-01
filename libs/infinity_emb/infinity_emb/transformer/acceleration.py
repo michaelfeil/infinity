049a86a585fe3e5e533e7426104f63996c9e1a1a
michaelfeil <me@michaelfeil.eu>
2024-03-31 18:55:34 -0700

    update defered moving to cpu

--- libs/infinity_emb/infinity_emb/transformer/acceleration.py
+++ libs/infinity_emb/infinity_emb/transformer/acceleration.py
@@ -1,12 +1,20 @@
 import os
+from typing import TYPE_CHECKING
 
 from infinity_emb._optional_imports import CHECK_OPTIMUM
 
 if CHECK_OPTIMUM.is_available:
-    from optimum.bettertransformer import BetterTransformer  # type: ignore
+    from optimum.bettertransformer import (  # type: ignore[import-untyped]
+        BetterTransformer,
+    )
+
+if TYPE_CHECKING:
+    from logging import Logger
+
+    from transformers import PreTrainedModel  # type: ignore[import-untyped]
 
 
-def to_bettertransformer(model, logger):
+def to_bettertransformer(model: "PreTrainedModel", logger: "Logger"):
     if os.environ.get("INFINITY_DISABLE_OPTIMUM", False):
         logger.info(
             "No optimizations via Huggingface optimum,"
@@ -18,8 +26,16 @@ def to_bettertransformer(model, logger):
     try:
         model = BetterTransformer.transform(model)
     except Exception as ex:
-        logger.exception(
-            f"BetterTransformer is not available for model. {ex}."
-            " Continue without bettertransformer modeling code."
-        )
+        # if level is debug then show the exception
+        if logger.level <= 10:
+            logger.exception(
+                f"BetterTransformer is not available for model: {model.__class__} {ex}."
+                " Continue without bettertransformer modeling code."
+            )
+        else:
+            logger.warning(
+                f"BetterTransformer is not available for model: {model.__class__}"
+                " Continue without bettertransformer modeling code."
+            )
+
     return model
