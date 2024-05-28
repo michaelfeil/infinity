7973f9cdbe6c9d7c0d3c69ef77507e4ee5f7a0c0
michaelfeil <me@michaelfeil.eu>
2024-05-28 09:05:01 -0700

    bump sentence-transformers

--- libs/infinity_emb/tests/unit_test/inference/test_models.py
+++ libs/infinity_emb/tests/unit_test/inference/test_models.py
@@ -44,7 +44,7 @@ def _pretrained_model_score(
         test_samples, name="sts-test"
     )
 
-    score = model.evaluate(evaluator) * 100
+    score = model.evaluate(evaluator)["sts-test_spearman_cosine"] * 100
     print(model_name, "{:.2f} vs. exp: {:.2f}".format(score, expected_score))
     assert score > expected_score or abs(score - expected_score) < 0.01
 
@@ -52,14 +52,14 @@ def _pretrained_model_score(
 @pytest.mark.parametrize(
     "model,score,compute_type",
     [
-        ("sentence-transformers/bert-base-nli-mean-tokens", 76.76, "int8"),
-        ("sentence-transformers/bert-base-nli-mean-tokens", 76.84, None),
-        ("sentence-transformers/all-MiniLM-L6-v2", 82.03, None),
-        ("sentence-transformers/all-MiniLM-L6-v2", 82.03, "default"),
-        ("sentence-transformers/all-MiniLM-L6-v2", 81.73, "int8"),
-        ("sentence-transformers/all-MiniLM-L6-v2", 82.03, "default"),
-        ("BAAI/bge-small-en-v1.5", 85.90, None),
-        ("BAAI/bge-small-en-v1.5", 85.90, "int8"),
+        ("sentence-transformers/bert-base-nli-mean-tokens", 76.60, "int8"),
+        ("sentence-transformers/bert-base-nli-mean-tokens", 76.46, None),
+        ("sentence-transformers/all-MiniLM-L6-v2", 81.03, None),
+        ("sentence-transformers/all-MiniLM-L6-v2", 81.03, "default"),
+        ("sentence-transformers/all-MiniLM-L6-v2", 80.73, "int8"),
+        ("sentence-transformers/all-MiniLM-L6-v2", 81.03, "default"),
+        ("BAAI/bge-small-en-v1.5", 84.90, None),
+        ("BAAI/bge-small-en-v1.5", 84.90, "int8"),
     ],
 )
 @pytest.mark.skipif(sys.platform == "darwin", reason="does not run on mac")
