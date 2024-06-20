10a0ef2412a834973e68849d0fdef74156c49312
michaelfeil <me@michaelfeil.eu>
2024-06-19 21:33:15 -0700

    fixes in webserver deployment

--- infra/modal/webserver.py
+++ infra/modal/webserver.py
@@ -1,7 +1,7 @@
 """Michael Feil, MIT License, 2024-06-17
 
 This is a reference implementation for infinity server via CLI.
-
+For deploying private endpoints/models, add the secrets (`INFINITY_API_KEY` and `HF_TOKEN`) to modal.
 """
 import subprocess
 import os
@@ -10,24 +10,20 @@ from modal import Image, App, gpu, web_server
 # Configuration
 PORT = 7997
 
-INFINITY_QUEUE_SIZE = 4096
-INFINITY_VERSION = os.environ.get("INFINITY_VERSION", "0.0.46")
+VERSION_INF = os.environ.get("VERSION_INF", "0.0.46")
 ENV = {
     # Per model args, padded by `;`
     "INFINITY_MODEL_ID": "jinaai/jina-clip-v1;michaelfeil/bge-small-en-v1.5;mixedbread-ai/mxbai-rerank-xsmall-v1;philschmid/tiny-bert-sst2-distilled;",
     "INFINITY_REVISION": "1cbe5e8b11ea3728df0b610d5453dfe739804aa9;ab7b31bd10f9bfbb915a28662ec4726b06c6552a;1d1a9dfbd0fde63df646402cf33e157e5852ead3;874eb28543ea7a7df80b6158bbf772d203efcab6;",
-    "INFINITY_MODEL_WARMUP": "false;",
-    "INFINITY_BATCH_SIZE": "16;",
+    "INFINITY_MODEL_WARMUP": "false;false;false;false;",
+    "INFINITY_BATCH_SIZE": "16;16;16;16;",
     # One-off args
-    "INFINITY_QUEUE_SIZE": str(INFINITY_QUEUE_SIZE),
+    "INFINITY_QUEUE_SIZE": "4096",
     "INFINITY_PORT": str(PORT),
-    "INFINITY_API_KEY": "",
-    "INFINITY_CI_DEPLOY": os.environ.get("INFINITY_CI_DEPLOY", ""),
 }
 CMD = "infinity_emb v2"
 
 MINUTES = 60  # in seconds
-
 
 
 def download_models():
@@ -35,8 +31,7 @@ def download_models():
     Ensures no downtime when huggingface is down.
     """
     print(f"downloading models {os.environ.get('INFINITY_MODEL_ID')}")
-    process = subprocess.Popen(CMD + " " + "--preload-only", shell=True)
-    exit_code = process.wait()
+    exit_code = subprocess.Popen(CMD + " " + "--preload-only", shell=True).wait()
     print("downloading models done.")
     assert exit_code == 0, f"Failed to download models. Exit code: {exit_code}"
 
@@ -46,7 +41,7 @@ def download_models():
 image = (
     Image.debian_slim(python_version="3.11")
     .pip_install(
-        f"infinity_emb[all]=={INFINITY_VERSION}", "numpy<2", "torchvision", "timm"
+        f"infinity_emb[all]=={VERSION_INF}",
     )
     .env(ENV)
     .run_function(
@@ -75,9 +70,7 @@ app = App("infinity", image=image)
     PORT,
     startup_timeout=5 * MINUTES,
     custom_domains=(
-      ["infinity.modal.michaelfeil.eu"]
-      if ENV["INFINITY_CI_DEPLOY"]
-      else []
+        ["infinity.modal.michaelfeil.eu"] if os.environ.get("CI_DEPLOY_INF", "") else []
     ),
 )
 def serve():
