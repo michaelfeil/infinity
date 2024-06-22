7c472bdcae85e61b27a6069561648422543f8340
michaelfeil <me@michaelfeil.eu>
2024-06-21 19:01:58 -0700

    fix engine sync shutdown

--- libs/infinity_emb/infinity_emb/sync_engine.py
+++ libs/infinity_emb/infinity_emb/sync_engine.py
@@ -83,10 +83,14 @@ class SyncEngineArray:
         async def block_until_engine_stop():
             logger.info("Started SyncEngineArray Background Event Loop")
             self._start_event.set()  # signal that the event loop has started
-            await self.async_engine_array.astart()
-            while not self._stop_event.is_set():
-                await asyncio.sleep(0.2)
-            await self.async_engine_array.astop()
+            try:
+                await self.async_engine_array.astart()
+                while not self._stop_event.is_set():
+                    await asyncio.sleep(0.2)
+            finally:
+                await self.async_engine_array.astop()
+            # additional delay to ensure that the engine is stopped
+            await asyncio.sleep(2.0)
 
         self._loop.run_until_complete(block_until_engine_stop())
         self._loop.close()
