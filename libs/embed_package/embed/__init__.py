f5ef3a59864cd8c418ce23fe41c48563e9a7da6d
michaelfeil <me@michaelfeil.eu>
2024-07-20 11:11:12 -0700

    embed_package version bump

--- libs/embed_package/embed/__init__.py
+++ libs/embed_package/embed/__init__.py
@@ -1,3 +1,6 @@
 from embed._infer import BatchedInference
+import importlib.metadata
 
-__all__ = ["BatchedInference"]
+__version__ = importlib.metadata.version("infinity_emb")
+
+__all__ = ["BatchedInference", "__version__"]
