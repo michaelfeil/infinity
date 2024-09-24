576cf99366ed3686bced99916a51406061d29ff1
michaelfeil <me@michaelfeil.eu>
2024-09-23 21:47:08 -0700

    move jpg image

--- libs/infinity_emb/tests/unit_test/test_engine.py
+++ libs/infinity_emb/tests/unit_test/test_engine.py
@@ -211,7 +211,7 @@ async def test_clap_like_model():
 
 @pytest.mark.anyio
 async def test_clip_embed_pil_image_input():
-    img_url = "https://github.com/michaelfeil/infinity/raw/3b72eb7c14bae06e68ddd07c1f23fe0bf403f220/libs/infinity_emb/tests/data/audio/beep.wav"
+    img_url = "https://github.com/michaelfeil/infinity/raw/06fd1f4d8f0a869f4482fc1c78b62a75ccbb66a1/docs/assets/cats_coco_sample.jpg"
     response = requests.get(img_url, stream=True)
     assert response.status_code == 200
     img_data = response.raw
