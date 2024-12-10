c335df818087158bcda5620d776d3cd4a7b87743
Wendy Mak <6398157+wwymak@users.noreply.github.com>
2024-12-10 07:09:00 +0000

    Adding optimum option for  PredictEngine (#492)
    * adding optimum onnx option for classification
    
    * removing a few unneeded bits from optimum.py
    
    * adding missing required args from optimum pipeline to make sure it gives the same output as torch implementation
    
    * adding unit test
    
    * remove a few stray things that are not needed
    
    * fixing code styles
    
    * minor linting fix
    
    * fix failing test
    
    ---------
    
    Co-authored-by: wendy mak <wendy.mak@stepstone.com>

---
+++ libs/infinity_emb/tests/unit_test/transformer/classifier/test_optimum_classifier.py
@@ -0,0 +1,49 @@
+import torch
+from optimum.pipelines import pipeline  # type: ignore
+from optimum.onnxruntime import ORTModelForSequenceClassification
+from infinity_emb.args import EngineArgs
+from infinity_emb.transformer.classifier.optimum import OptimumClassifier
+
+
+def test_classifier(model_name: str = "SamLowe/roberta-base-go_emotions-onnx"):
+    model = OptimumClassifier(
+        engine_args=EngineArgs(
+            model_name_or_path=model_name,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+        )  # type: ignore
+    )
+
+    pipe = pipeline(
+        task="text-classification",
+        model=ORTModelForSequenceClassification.from_pretrained(
+            model_name, file_name="onnx/model_quantized.onnx"
+        ),
+        top_k=None,
+    )
+
+    sentences = ["This is awesome.", "I am depressed."]
+
+    encode_pre = model.encode_pre(sentences)
+    encode_core = model.encode_core(encode_pre)
+    preds = model.encode_post(encode_core)
+
+    assert len(preds) == len(sentences)
+    assert isinstance(preds, list)
+    assert isinstance(preds[0], list)
+    assert isinstance(preds[0][0], dict)
+    assert isinstance(preds[0][0]["label"], str)
+    assert isinstance(preds[0][0]["score"], float)
+    assert preds[0][0]["label"] == "admiration"
+    assert 0.98 > preds[0][0]["score"] > 0.93
+
+    preds_orig = pipe(sentences, top_k=None, truncation=True)
+
+    assert len(preds_orig) == len(preds)
+
+    for pred_orig, pred in zip(preds_orig, preds):
+        assert len(pred_orig) == len(pred)
+        for pred_orig_i, pred_i in zip(pred_orig[:5], pred[:5]):
+            assert abs(pred_orig_i["score"] - pred_i["score"]) < 0.05
+
+            if pred_orig_i["score"] > 0.005:
+                assert pred_orig_i["label"] == pred_i["label"]
